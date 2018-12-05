d7436f53f0eb972667157e20780202370bbe9af3
priyanka <priyankapanda00@gmail.com>
2018-12-05 16:15:30 +0530

    nothing

--- code/hello.java
+++ code/hello.java
@@ -1,5 +1,9 @@
 public class hello{
 public static void main(String args[]){
 System.out.println("initialize local directory with git init");
+System.out.println("add the file to staging area with git add");
+System.out.println("add the file to local repo with git commit");
+System.out.println("set the remote repo where local repo should be pushed git remote add origin remote repo url");
+System.out.println("push the file to remote repo with git push");
 }
 }
