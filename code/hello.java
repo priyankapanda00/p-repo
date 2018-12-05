4e79782d8e9d8145571149d3d81571dfa67a1bbb
priyanka <priyankapanda00@gmail.com>
2018-12-05 18:01:34 +0530

    removed spaces

--- code/hello.java
+++ code/hello.java
@@ -5,10 +5,5 @@ System.out.println("add the file to staging area with git add");
 System.out.println("add the file to local repo with git commit");
 System.out.println("set the remote repo where local repo should be pushed git remote add origin remote repo url");
 System.out.println("push the file to remote repo with git push");
-
-
-
-
-
 }
 }
